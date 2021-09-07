da78e8a647f942771479d4d3951c3de2e7e45b21
Sandesh9834 <78275234+Sandesh9834@users.noreply.github.com>
2021-09-08 00:18:33 +0530

    commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var txtInput = document.querySelector("#txt1")
+var btnTranslate = document.querySelector("#btn-translate");
+var outputText = document.querySelector("#output")
+
+var serverURL = "	https://api.funtranslations.com/translate/dothraki.json";
+
+function translate(text)
+{
+     return serverURL + "?" + "text=" + text;
+}
+
+
+function clickHandler ()
+{     
+      console.log("Button clicked");
+      console.log("Entered text is: "+ txtInput.value)
+      var inputTxt = txtInput.value;
+
+      fetch(translate(inputTxt))
+            .then(response => response.json())
+            .then (json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText;
+            //console.log(json.contents.translated)
+            })
+      
+}
+
+btnTranslate.addEventListener("click", clickHandler)
